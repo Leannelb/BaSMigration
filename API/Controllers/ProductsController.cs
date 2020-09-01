e027cb2ffa3b614ac95eecd947b53f9e33a5b69a
Leanne Lacey-Byrne <llacey-byrne@saadian.com>
2020-09-01 18:42:15 +0100

    all dependancies set and core and infastructure set up

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,10 @@
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace API.Controllers
 {
@@ -6,15 +12,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return "this is a list of products";
+            _context = context;
         }
-        [HttpGet("{id}")]
-        public string GetProducts(int id)
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return "this is a product";
+            var products = await  _context.Products.ToListAsync();
+
+            return Ok(products);
+            }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _context.Products.FindAsync(id);
         }
     }
 }

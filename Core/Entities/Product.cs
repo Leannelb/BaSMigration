d58f21c84c909c11d34f2de269f62a74fd682db9
Leanne Lacey-Byrne <llacey-byrne@saadian.com>
2020-09-03 19:52:10 +0100

    Adding relationships

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,12 +1,22 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
         //type prop and enter and the below line is wrote for you
-        public int Id { get; set; }
+       //gets id from BaseEntity
 
         //naming convention is PascalCase
         public string Name { get; set; }
 
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+        public int ProductTypeId { get; set; }
+
+        public ProdutBrand ProductBrand { get; set; }
+
+        public int ProductBrandId { get; set; }
     }
 }
---
+++ Core/Entities/ProdutBrand.cs
@@ -0,0 +1,7 @@
+namespace Core.Entities
+{
+    public class ProdutBrand : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}

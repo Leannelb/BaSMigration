4b8163ae1bf2164d19e1d9e1ac36bb6bd605b5c8
leannelb <leannelaceybyrne@outlook.com>
2021-01-17 16:32:14 +0000

    s89 retriving products from backend api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagingation } from './models/pagination';
-import { IProduct } from './models/product';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +8,11 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe(
-      (response: IPagingation) => {
-      console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
-  }
+  ngOnInit(): void {}
+
 }
 
 

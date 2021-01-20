985d45a7e5f9a429c9da435551211704c20f2609
leannelb <leannelaceybyrne@outlook.com>
2021-01-20 19:50:38 +0000

    95. filters operational

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,14 +11,55 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected: number;
+  typeIdSelected: number;
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe(response => {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  // tslint:disable-next-line: typedef
+  getProducts() {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
       this.products = response.data;
     }, error => {
       console.log('error shop component from service call ', error);
     });
+  }
+
+  // tslint:disable-next-line: typedef
+  getBrands() {
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+        console.log('error from get brands API call ', error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  getTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      this.types = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log('error from types API call ', error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  onBrandsSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  // tslint:disable-next-line: typedef
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
   }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagingation } from '../shared/models/pagination';
-
+import { IType } from '../shared/models/productType';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,11 +11,41 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
 
   constructor(private http: HttpClient) { }
+  // tslint:disable-next-line: typedef
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get<IPagingation>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+    }
+    // this way were getting back an observable which we can interact with to project the body
+    // into the iPagination object, therefore we use an RXJS method pipe()
+    // to map it into an iPagination object
+    // we use pipe to wrap any rxjs operators, we can add multipe rxjs operators in one pipe to manipulate the observable.
+
+  // doing it like this returns us the http response
+  // return this.http.get<IPagingation>(this.baseUrl + 'products', {observe: 'response', params});
+  // as oppoesed to this which gives us the body of the response automatically
+  // return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
 
   // tslint:disable-next-line: typedef
-  getProducts() {
-    return this.http.get<IPagingation>(this.baseUrl + 'products?pageSize=50'); // this returns all of them (or the max 50 if we had 50)
-    // return this.http.get<IPagingation>(this.baseUrl + 'products'); // this returns less items - 6 as we specified that as the default fro retival from the A?I
+  getTypes() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
 
+  // tslint:disable-next-line: typedef
+  getBrands() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }

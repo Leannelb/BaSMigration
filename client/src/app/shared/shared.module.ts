b1f444e706f3b12ce41626b2e73d0b0c1f2a1c23
leannelb <leannelaceybyrne@outlook.com>
2021-01-21 08:49:38 +0000

    97. pagination in progress using bootstraps given pagination module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    PaginationModule.forRoot()
+  ],
+  // we add for root because the PaginationModule has it's own providers array that need to be injected into the root at startup.
+  exports: [
+    PaginationModule
   ]
 })
 export class SharedModule { }

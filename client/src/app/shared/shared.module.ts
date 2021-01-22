7f1e0a21765c4cf032475726e5cef67281a36698
leannelb <leannelaceybyrne@outlook.com>
2021-01-22 22:44:13 +0000

    101 complete. INPUTS HOW THEY WORK paging header added to its own component and used via imports

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PagingHeaderComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
   // we add for root because the PaginationModule has it's own providers array that need to be injected into the root at startup.
   exports: [
-    PaginationModule
+    PaginationModule,
+    PagingHeaderComponent
   ]
 })
 export class SharedModule { }

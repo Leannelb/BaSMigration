d598f729f878d303c23cd6c8ed86519aaebac97f
leannelb <leannelaceybyrne@outlook.com>
2021-02-02 08:45:45 +0000

    141 4.39s redis install not working come back to this

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -16,6 +16,8 @@ namespace API.Controllers
 // redis is just local temporary storage for a month
 // redis adds information to a snapshot via disk uplaod every minute, 
 // so if the sercer was to go down it would still have the information.
+// lesson 141 didnt work due to:
+// workaround with https://www.c-sharpcorner.com/article/installing-redis-cache-on-windows/
         [HttpGet]
         public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
         {

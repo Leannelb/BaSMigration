804ac8e0ec765fbee77e4061d10c8c00bb3ea0a0
leannelb <leannelaceybyrne@outlook.com>
2021-02-07 21:04:57 +0000

    149 complete

--- client/src/app/shared/models/basket.ts
+++ client/src/app/shared/models/basket.ts
@@ -17,6 +17,6 @@ export interface IBasketItem {
 
 export class Basket implements IBasket {
     id = uuidv4();
-    items: IBasketItem[];
+    items: IBasketItem[] = [];
 }
 // installed npm install uuid to get unique IDs for baskets

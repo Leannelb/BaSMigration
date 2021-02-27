f50a14416f2b338bee2c5480254c832324001f40
leannelb <leannelaceybyrne@outlook.com>
2021-02-27 11:05:36 +0000

    181 adding email validation

--- API/DataTransferObjects/RegisterDto.cs
+++ API/DataTransferObjects/RegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DataTransferObjects
 {
     public class RegisterDto
     {
-        public string DisplayName {get; set;}
-        public string Email {get; set;}
-        public string Password {get; set;}
+       [Required]
+       public string DisplayName {get; set;}
+       [Required]
+       public string Email {get; set;}
+       [Required]
+       // got the regex from https://regexlib.com/Search.aspx?k=password and searched for password
+       // this gives suggested Regex's
+       
+       [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$")]
+       public string Password {get; set;}
     }
 }

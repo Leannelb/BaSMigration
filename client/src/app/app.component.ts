e6779d5f67913730db5facabf711f297d78b9258
leannelb <leannelaceybyrne@outlook.com>
2021-03-02 18:46:59 +0000

    persisting the login is working now but the basket method doesnt seem to be running so will come back here if needed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
   title = 'client';
 
   ngOnInit(): void {
-      const basketId = localStorage.getItem('basket_id');
- if(basketId) {
-    this.basketService.getBasket(basketId).subscribe(() => {
-      console.log('initalised basket');
-    }, error => {
-      console.log('error ' , error);
-    });
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  // tslint:disable-next-line: typedef
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(
+        () => {
+          console.log('loaded user');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+  // tslint:disable-next-line: typedef
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('>>>initalised basket');
+        },
+        (error) => {
+          console.log('error BASKET error ', error);
+        }
+      );
     }
   }
 }
-

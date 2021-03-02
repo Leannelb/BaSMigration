0ebfd614fd17aa3d7325912514af0f9c8171fe3d
leannelb <leannelaceybyrne@outlook.com>
2021-03-02 17:45:15 +0000

    191 login form set up need to submit to server

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  // tslint:disable-next-line: typedef
+  createLoginForm() {
+    this.loginForm = new FormGroup ({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  onSubmit() {
+    console.log('this.loginForm.controls ', this.loginForm.controls);
+    console.log({this: this.loginForm});
   }
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { OrderTotalsComponent } from './components/order-totals/order-totals.com
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }

cb39f263a447c0068161e69bebbeb57d8f5187c0
Leanne Lacey-Byrne <llacey-byrne@saadian.com>
2021-03-25 08:25:36 +0000

    setting up on work laptop bas project;

---
+++ API/Helpers/OrderItemUrlResolver.cs
@@ -0,0 +1,27 @@
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+using API.DataTransferObjects;
+using Microsoft.Extentions.Configuration;
+
+
+namespace API.Helpers
+{
+    public class OrderItemUrlResolver : IValueResolver<OrderItem, OrderItemDto, string>
+    {
+        private readonly IConfiguration _config;
+        public OrderItemUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+         if(!string.IsNullOrEmpty(source.ItemOrdered.PictureUrl))
+            {
+                return _config["ApiUrl"] + source.ItemOrdered.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}

9ec810877fe9e3dca4a206fef72bbc5b38d32e8d
leannelb <leannelaceybyrne@outlook.com>
2021-04-11 19:54:35 +0100

    253 solution to orders exersise

--- client/src/app/orders/orders.module.ts
+++ client/src/app/orders/orders.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { OrderDetailedComponent } from './order-detailed/order-detailed.component';
+import { OrdersRoutingModule } from '../basket-routing.module';
 
 
 
 @NgModule({
   declarations: [OrdersComponent, OrderDetailedComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    OrdersRoutingModule
   ]
 })
 export class OrdersModule { }
